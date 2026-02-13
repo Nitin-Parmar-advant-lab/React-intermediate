f39c496bab8975ecc471236370d36e0f255ec411
1-and-ME <nitin.oneand@gmail.com>
2026-02-13 17:08:47 +0530

    time game pronject done

--- src/components/ResultModel.jsx
+++ src/components/ResultModel.jsx
@@ -1,28 +1,39 @@
 import { useImperativeHandle, useRef } from "react";
+import { createPortal } from 'react-dom'
 
-export default function ResultModel({ ref, result, targetTime }) {
-const dial = useRef();
-
+export default function ResultModel({
+    ref,
+    remainingTime,
+    targetTime,
+    onReset,
+}) {
+    const dial = useRef();
+    const useLost = remainingTime <= 0;
+    const formattedRestTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
     useImperativeHandle(ref, () => {
         return {
             open() {
                 dial.current.showModal();
-            }
-        }
+            },
+        };
     });
 
-    return (
-        <dialog className="result-modal" ref={dial}>
-            <h2>You {result}</h2>
+    return createPortal(
+        <dialog className="result-modal" ref={dial} onClose={onReset}>
+            {useLost && <h2>You lost</h2>}
+            {!useLost && <h2>Your Score: {score}</h2>}
             <p>
                 The target time was <strong>{targetTime} seconds.</strong>
             </p>
             <p>
-                You stopped the timer with <strong>X second left.</strong>
+                You stopped the timer with <strong>{formattedRestTime}</strong>{" "}
+                seconds left.
             </p>
             <form method="dialog">
-                <button>Close</button>
+                <button onClick={onReset}>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById("modal")
     );
 }
